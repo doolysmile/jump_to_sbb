9c25faca2eab443f2be29e80b64282769170ef78
KCH <doolysmile1@naver.com>
2022-08-20 00:48:54 +0900

    feat : detail을 위한 기능

--- src/main/java/com/ll/exam/sbb/question/QuestionService.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionService.java
@@ -1,18 +1,29 @@
 package com.ll.exam.sbb.question;
 
+import com.ll.exam.sbb.DataNotFoundException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class QuestionService {
-    @Autowired
-    private QuestionRepository questionRepository;
+    private final QuestionRepository questionRepository;
 
-    public Question findById(int id) {
-        Question q1 = questionRepository.findById(2).get();
-        Question q2 = questionRepository.findById(2).get();
-        System.out.println(q2.getAnswerList());
+    public List<Question> getList() {
+        return this.questionRepository.findAll();
+    }
 
-        return q2;
+    public Question getQuestion(int id) {
+        Optional<Question> oq = questionRepository.findById(id);
+
+        if ( oq.isPresent() ) {
+            return oq.get();
+        }
+
+        throw new DataNotFoundException("question not found");
     }
 }
