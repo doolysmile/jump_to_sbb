b150dcf597580202d909eee7fa882f868fb2ea2a
KCH <doolysmile1@naver.com>
2022-08-22 03:21:58 +0900

    feat : question에 validation적용

--- src/main/java/com/ll/exam/sbb/question/QuestionController.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionController.java
@@ -3,21 +3,36 @@ package com.ll.exam.sbb.question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.List;
 
-@Controller
 @RequestMapping("/question")
-@RequiredArgsConstructor
+@Controller
+@RequiredArgsConstructor // 생성자 주입
+// 컨트롤러는 Repository가 있는지 몰라야 한다.
+// 서비스는 웹브라우저라는것이 이 세상에 존재하는지 몰라야 한다.
+// 리포지터리는 서비스를 몰라야 한다.
+// 서비스는 컨트롤러를 몰라야 한다.
+// DB는 리포지터리를 몰라야 한다.
+// SPRING DATA JPA는 MySQL을 몰라야 한다.
+// SPRING DATA JPA(리포지터리) -> JPA -> 하이버네이트 -> JDBC -> MySQL Driver -> MySQL
 public class QuestionController {
-
+    // @Autowired // 필드 주입
     private final QuestionService questionService;
 
     @RequestMapping("/list")
+    // 이 자리에 @ResponseBody가 없으면 resources/question_list/question_list.html 파일을 뷰로 삼는다.
     public String list(Model model) {
         List<Question> questionList = questionService.getList();
 
+        // 미래에 실행된 question_list.html 에서
+        // questionList 라는 이름으로 questionList 변수를 사용할 수 있다.
         model.addAttribute("questionList", questionList);
 
         return "question_list";
@@ -33,30 +48,17 @@ public class QuestionController {
     }
 
     @GetMapping("/create")
-    public String questionCreate() {
+    public String questionCreate(QuestionForm questionForm) {
         return "question_form";
     }
 
     @PostMapping("/create")
-    public String questionCreate(Model model, QuestionFrom questionFrom) {
-        boolean hasError = false;
-
-        if (questionFrom.getSubject() == null || questionFrom.getSubject().trim().length() == 0) {
-            model.addAttribute("subjectErrorMsg", "제목 좀...");
-            hasError = true;
-        }
-
-        if (questionFrom.getContent() == null || questionFrom.getContent().trim().length() == 0) {
-            model.addAttribute("contentErrorMsg", "내용 좀...");
-            hasError = true;
-        }
-
-        if (hasError) {
-            model.addAttribute("questionFrom", questionFrom);
+    public String questionCreate(Model model, @Valid QuestionForm questionForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
             return "question_form";
         }
 
-        questionService.create(questionFrom.getSubject(), questionFrom.getContent());
+        questionService.create(questionForm.getSubject(), questionForm.getContent());
         return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
     }
 }
